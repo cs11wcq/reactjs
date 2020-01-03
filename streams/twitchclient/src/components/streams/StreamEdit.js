e299508aea420d092151431f92b9a4d3985e4924
Kim, Seth <syk106@ucsd.edu>
2020-01-03 15:53:28 -0800

    streams s

--- streams/twitchclient/src/components/streams/StreamEdit.js
+++ streams/twitchclient/src/components/streams/StreamEdit.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {editStream} from '../../actions/index';
+import {fetchStream} from '../../actions/index';
 
 //props.match.params.id gives us the id of the user in the url
 //ie: localhost:3000/streams/edit/:id
-const StreamEdit = (props) => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-};
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    //console.log('props',this.props);
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 
 //select appropriate stream with the id given by ownProps
 const mapStateToProps = (state, ownProps) => {
@@ -15,5 +25,6 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(mapStateToProps, {
-  editStream: editStream
+  editStream: editStream,
+  fetchStream: fetchStream
 })(StreamEdit);
